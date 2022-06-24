8ab306ab9f1b13e36177dd8b7e5926bcb0623c4e
mmchokor <mohameddd701@gmail.com>
2022-06-25 01:36:46 +0300

    18. Frontend Authentication - 5

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 function Register() {
    const [formData, setFormData] = useState({
@@ -12,6 +14,12 @@ function Register() {
 
    const { name, email, password, password2 } = formData
 
+   const dispatch = useDispatch()
+
+   const { user, isLoading, isSuccess, message } = useSelector(
+      (state) => state.auth
+   )
+
    const onChange = (e) => {
       setFormData((prevState) => ({
          ...prevState,
@@ -20,18 +28,25 @@ function Register() {
    }
 
    const onSubmit = (e) => {
-    e.preventDefault()
+      e.preventDefault()
 
-    if (password !== password2) {
-        toast.error('Passwords do not match')
-    }
+      if (password !== password2) {
+         toast.error('Passwords do not match')
+      } else {
+         const userData = {
+            name,
+            email,
+            password,
+         }
+         dispatch(register(userData))
+      }
    }
 
    return (
       <>
          <section className='heading'>
             <h1>
-               <FaUser /> Register
+               <FaUser /> Register {user}
                <p>Please create an account</p>
             </h1>
          </section>
@@ -86,8 +101,8 @@ function Register() {
                      required
                   />
                </div>
-               <div className="form-group">
-                <button className="btn btn-block">Submit</button>
+               <div className='form-group'>
+                  <button className='btn btn-block'>Submit</button>
                </div>
             </form>
          </section>
