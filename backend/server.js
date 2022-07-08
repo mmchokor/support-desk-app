ba4a23ca21e07cbb919afb3e1c463b8a7b4b11e8
mmchokor <mohameddd701@gmail.com>
2022-07-08 15:37:35 +0300

    deploying to heroku

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
-const cors = require('cors');
+const cors = require('cors')
+const path = require('path')
 
 // connect to db
 connectDB()
@@ -14,10 +15,10 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-   res.status(200).json({message: 'Welcome to support desk API'})
+   res.status(200).json({ message: 'Welcome to support desk API' })
 })
 
 // Routes
@@ -26,6 +27,14 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 
+// deployment
+__dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '/frontend/dist')))
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+   })
+}
 app.listen(PORT, () => {
    console.log(`Server started on port ${PORT}`)
 })
