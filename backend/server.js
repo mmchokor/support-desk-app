8c4280492b5ebc38fda81901400a9b4406357f1b
mmchokor <mohameddd701@gmail.com>
2022-07-08 17:47:43 +0300

    heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 const path = require('path')
 
 // connect to db
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -27,7 +30,7 @@ app.use(errorHandler)
 // deployment
 __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '/frontend/dist')))
+   app.use(express.static(path.join(__dirname, '\frontend\dist')))
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
    })
