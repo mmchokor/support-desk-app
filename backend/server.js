1f565e3cd91ff29b76bf39a3b6c80acc2e01deb3
mmchokor <mohameddd701@gmail.com>
2022-07-08 17:56:47 +0300

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(errorHandler)
 // deployment
 __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '\frontend\dist')))
+   app.use(express.static(path.join(__dirname, '/frontend/dist')))
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
    })
